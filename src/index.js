52b57fca07ac7ed8d60bb6dcefcd9bc36f225825
Eric Wooley <Eric.Wooley@rosetta.com>
2016-04-27 00:04:02 -0700

    use index.js as main file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import rs from './reduction-sauce'
+export const reductionSauce = rs
+export {
+    default as reductionReducer,
+    actions as sauceActions
+} from './reduction-reducer'
+export default reductionSauce

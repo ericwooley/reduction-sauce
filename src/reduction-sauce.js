6ae1c4407b37e76f0f0d0ca2d4e40aa051f88904
Eric Wooley <Eric.Wooley@rosetta.com>
2016-05-23 09:53:43 -0700

    Got rid of undefined key

--- src/reduction-sauce.js
+++ src/reduction-sauce.js
@@ -7,13 +7,14 @@ function reductionSauce ({
   return (Comp) => {
     const Sauced = connect(
       (state, props) => {
-        return {...(state.reductionReducer[(key + props.sauceKey)] || {})}
+        return {...(state.reductionReducer[(key + (props.sauceKey || ''))] || {})}
       },
       (dispatch, props) => {
+        const sauceKey = props.sauceKey || ''
         return {
-          setSauceKey: (...setSauceArgs) => dispatch(actions.setSauceKey(key + props.sauceKey, ...setSauceArgs)),
-          setSauce: (...setSauceArgs) => dispatch(actions.setSauce(key + props.sauceKey, ...setSauceArgs)),
-          resetSauce: (...setSauceArgs) => dispatch(actions.resetSauce(key + props.sauceKey, ...setSauceArgs))
+          setSauceKey: (...setSauceArgs) => dispatch(actions.setSauceKey(key + sauceKey, ...setSauceArgs)),
+          setSauce: (...setSauceArgs) => dispatch(actions.setSauce(key + sauceKey, ...setSauceArgs)),
+          resetSauce: (...setSauceArgs) => dispatch(actions.resetSauce(key + sauceKey, ...setSauceArgs))
         }
       })(Comp)
     return connect(mapStateToProps, mapDispatchProps, ...args)(Sauced)
